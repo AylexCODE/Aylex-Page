68ddb310a41f88fcb83f3dce2155f600eb8b1f0a
Alex <130986014+AylexCODE@users.noreply.github.com>
2023-08-16 21:29:32 +0800

    Add files via upload

--- home/additional_features/showSocials.js
+++ home/additional_features/showSocials.js
@@ -31,3 +31,19 @@ const googleShowsss = document.querySelector(".googleShowsss");
             googleShowsss.classList.remove("active");
         }, 12000);
     }
+
+const project1 = document.querySelector(".project1-content");
+const project1Detect = document.querySelector(".project1");
+    
+    function hoverProject1() {
+        project1.classList.add("hover");
+        project1Detect.classList.add("hover");
+    }
+    
+    function hoverProject01() {
+        project1.classList.remove("hover");
+        project1Detect.classList.remove("hover");
+    }
+    
+project1Detect.addEventListener("mouseover", hoverProject1);
+project1Detect.addEventListener("mouseout", hoverProject01);
