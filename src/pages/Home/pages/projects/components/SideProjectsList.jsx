df3815aec1c0f5ac95bd3f9793722eefecaf6c17
Alex <130986014+AylexCODE@users.noreply.github.com>
2025-10-04 10:56:32 +0000

    Working on displaying Side Projects

--- src/pages/Home/pages/projects/components/SideProjectsList.jsx
+++ src/pages/Home/pages/projects/components/SideProjectsList.jsx
@@ -1,4 +1,13 @@
+import OctoKitRest from "../utilities/octoKit";
+import { useEffect } from "react";
+
 export default function SideProjectsList(){
+    useEffect(() => {
+        (async function(){
+            const data = await new OctoKitRest().getSideProjectsFiles();
+        })();
+        // eslint-disable-next-line
+    }, []);
     return (
         <div className="pt-[1rem] w-full">
             <p className="opacity-60">Side Projects</p>
