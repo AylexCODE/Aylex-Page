52791c04cd2751961ba837f4715d5251f1dbf068
Alex <130986014+AylexCODE@users.noreply.github.com>
2025-10-06 12:45:04 +0800

    Changed bio

--- src/pages/Home/pages/about/components/AboutMe.jsx
+++ src/pages/Home/pages/about/components/AboutMe.jsx
@@ -21,7 +21,7 @@ export default function AboutMe(){
     return (
         <div className="mt-[0.75rem]">
             <span ref={text} className="block anim-size transition-all duration-250 overflow-hidden">
-                <p>I'm a college student at BISU Balilihan. I started coding in the year 2017 but had to stop due to limitations in accessing resources. However, I resumed coding in the year 2022, and {isMore ? `up until now (${date}), I can say` : "..."} <span className={isMore ? "inline" : "hidden"}>I'm competent but never better at <Typewriter text={["JavaScript", "PHP", "MySQL", "TailwindCSS"]}/></span></p>
+                <p>Hey there! I'm Alexander I. Jala, I specialize in building powerful backend systems. Passionate about efficient algorithms, and clean architecture {isMore ? `. Now (${date}), I can say` : "..."} <span className={isMore ? "inline" : "hidden"}>I'm competent but never better at <Typewriter text={["JavaScript", "PHP", "MySQL", "TailwindCSS"]}/></span></p>
                 <p className={`mt-[0.5rem] ${isMore ? "block" : "hidden"}`}>I'm excited to make the leap and continue refining my skills in the world of programming.</p>
             </span>
             <button className="underline opacity-70" onClick={() => handleSeeMore()}>{isMore ? "See Less" : "See More"}</button>
