d06f07ba7280245beb22b704fc1cde23cd601368
Alex <130986014+AylexCODE@users.noreply.github.com>
2025-10-06 10:36:59 +0000

    Improved bio

--- src/pages/Home/pages/about/components/AboutMe.jsx
+++ src/pages/Home/pages/about/components/AboutMe.jsx
@@ -21,7 +21,7 @@ export default function AboutMe(){
     return (
         <div className="mt-[0.75rem]">
             <span ref={text} className="block anim-size transition-all duration-250 overflow-hidden">
-                <p>Hey there! I'm Alexander I. Jala, I specialize in building powerful backend systems. Passionate about efficient algorithms, and clean architecture {isMore ? `. Now (${date}), I can say` : "..."} <span className={isMore ? "inline" : "hidden"}>I'm competent but never better at <Typewriter text={["JavaScript", "PHP", "MySQL", "TailwindCSS"]}/></span></p>
+                <p>Hey there! I'm Alexander I. Jala, I specialize in building powerful backend systems. Passionate about efficient algorithms, and clean architecture{isMore ? `. Now (${date}), I can say` : "..."} <span className={isMore ? "inline" : "hidden"}>I'm competent but never better at <Typewriter text={["JavaScript", "PHP", "MySQL", "TailwindCSS"]}/></span></p>
                 <p className={`mt-[0.5rem] ${isMore ? "block" : "hidden"}`}>I'm excited to make the leap and continue refining my skills in the world of programming.</p>
             </span>
             <button className="underline opacity-70" onClick={() => handleSeeMore()}>{isMore ? "See Less" : "See More"}</button>
