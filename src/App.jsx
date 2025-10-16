777b4e6ffc659b00e9c96d10a3be2c1d50c8260c
Alex <130986014+AylexCODE@users.noreply.github.com>
2025-10-16 10:15:07 +0000

    Added Route for Games

--- src/App.jsx
+++ src/App.jsx
@@ -9,27 +9,34 @@ import Main from './pages/Home/pages/main/Main';
 import About from './pages/Home/pages/about/About';
 import Projects from './pages/Home/pages/projects/Projects';
 import ProjectsFile from './pages/Home/pages/projects/ProjectsFile';
+import Clipboard from './pages/Home/components/clipboard/clipboard';
 
 import ClashofClans from './pages/Clash-of-Clans/index';
 import Minecraft from './pages/Minecraft/index';
-import Clipboard from './pages/Home/components/clipboard/clipboard';
+
+import Games from './pages/Games';
+import NumberLock from './pages/Games/numberlock';
 
 function App(){
     return (
-    <HashRouter>
-      <Routes>
-        <Route path="*" element={<PageNotFound /> } />
-        <Route path="/" element={<Home />} >
-            <Route index element={<Main />} />
-            <Route path="about" element={<About />} />
-            <Route path="projects" element={<Projects />} />
-        </Route>
-        <Route path="projects/:file" element={<ProjectsFile />} />
-        <Route path="/clash-of-clans" element={<ClashofClans />} />
-        <Route path="/minecraft" element={<Minecraft />} />
-        <Route path="/clipboard" element={<Clipboard />} />
-      </Routes>
-    </HashRouter>
+        <HashRouter>
+            <Routes>
+                <Route path="*" element={<PageNotFound /> } />
+                <Route path="/" element={<Home />} >
+                    <Route index element={<Main />} />
+                    <Route path="about" element={<About />} />
+                    <Route path="projects" element={<Projects />} />
+                </Route>
+                <Route path="projects/:file" element={<ProjectsFile />} />
+                <Route path="/clash-of-clans" element={<ClashofClans />} />
+                <Route path="/minecraft" element={<Minecraft />} />
+                <Route path="/clipboard" element={<Clipboard />} />
+                <Route path="/games">
+                    <Route index element={<PageNotFound />} />
+                    <Route path="numberlock" element={<NumberLock />} />
+                </Route>
+            </Routes>
+        </HashRouter>
     );
 }
 
