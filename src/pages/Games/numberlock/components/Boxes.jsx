e6530916d434afadbcf981161d171af80d473fab
Alex <130986014+AylexCODE@users.noreply.github.com>
2025-10-18 11:01:46 +0000

    Fixed issue where boxes with value shrinks

--- src/pages/Games/numberlock/components/Boxes.jsx
+++ src/pages/Games/numberlock/components/Boxes.jsx
@@ -17,7 +17,7 @@ export default function Boxes(props){
     }, []);
 
     return (
-        <section ref={guessBox} className="w-[calc(100%-3rem)] flex flex-row flex-wrap justify-center gap-[1rem] p-[1rem] z-1 [&>span>p]:opacity-50 [&>span>p]:grid [&>span>p]:place-items-center [&>span>p]:size-[50px] [&>span]:border [&>span]:border-t-[2px] [&>span]:border-r-[5px] [&>span]:border-b-[5px] [&>span]:border-l-[2px]">
+        <section ref={guessBox} className="w-[calc(100%-3rem)] flex flex-row flex-wrap justify-center gap-[1rem] p-[1rem] z-1 [&>span>p]:opacity-50 [&>span>p]:grid [&>span>p]:place-items-center [&>span>p]:size-[50px] [&>span]:border [&>span]:border-t-[2px] [&>span]:border-r-[5px] [&>span]:border-b-[5px] [&>span]:border-l-[2px] [&>span>h2]:size-[50px] [&>span>h2]:grid [&>span>h2]:place-items-center [&>span>h2]:text-[1.5rem] [&>span>h2]:font-bold">
             {boxElements}
         </section>
     )
