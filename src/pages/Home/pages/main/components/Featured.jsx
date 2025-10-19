d238cd243c0fa0502cfa2942ea7fe66e0f32ad0e
Alex <130986014+AylexCODE@users.noreply.github.com>
2025-10-19 11:37:25 +0000

    Major update
    Separated featured projects from normal projects

--- src/pages/Home/pages/main/components/Featured.jsx
+++ src/pages/Home/pages/main/components/Featured.jsx
@@ -1,7 +1,16 @@
+import { useEffect, useRef } from "react";
+
 export default function Featured(props){
+    const featuredList = useRef(null);
+        
+    useEffect(() => {
+        props.featured(featuredList.current.children.length);
+        // eslint-disable-next-line
+    }, []);
+
     return (
         <div className="pt-[1rem] w-full">
-            <ul className="w-full snap-x snap-mandatory h-[20rem] overflow-x-scroll flex flex-row gap-[0.5rem] [&>li]:snap-center [&>li]:block [&>li]:h-full [&>li]:w-[20rem] [&>li]:border [&>li]:border-borderColor [&>li]:rounded-xl [&>li>span]:block [&>li>span]:p-[0.5rem] [&>li>div]:h-[15rem] [&>li>div]:w-[20rem] [&>li>div]:rounded-t-xl [&>li>div]:overflow-hidden [&>li>div>img]:h-full [&>li>div>img]:w-full [&>li>span>p]:first:font-medium">
+            <ul ref={featuredList} className="w-full snap-x snap-mandatory h-[20rem] overflow-x-scroll flex flex-row gap-[0.5rem] [&>li]:snap-center [&>li]:block [&>li]:h-full [&>li]:w-[20rem] [&>li]:border [&>li]:border-borderColor [&>li]:rounded-xl [&>li>span]:block [&>li>span]:p-[0.5rem] [&>li>div]:h-[15rem] [&>li>div]:w-[20rem] [&>li>div]:rounded-t-xl [&>li>div]:overflow-hidden [&>li>div>img]:h-full [&>li>div>img]:w-full [&>li>span>p]:first:font-medium">
                 <li>
                     <div><img src="%PUBLIC_URL%/../DiscordBOT-Handle.webp" alt="project handle"></img></div>
                     <span>
--- src/pages/Home/pages/projects/components/ProjectsList.jsx
+++ src/pages/Home/pages/projects/components/ProjectsList.jsx
@@ -1,29 +1,29 @@
 import { useEffect, useRef } from "react";
+import { NavLink } from "react-router-dom";
 
 export default function ProjectsList(props){
-    const projectsList = useRef();
+    const projectsList = useRef(null);
     
     useEffect(() => {
         props.project(projectsList.current.children.length);
         // eslint-disable-next-line
     }, []);
+
+    const calendarIcon = (
+        <svg width="16px" height="16px" viewBox="0 0 16 16"><path fill="#FFFFFF" fill-opacity="0.0" stroke="#000000" strokeWidth="1.5" stroke-opacity="1.0" stroke-miterlimit="10" d="M1.5,13.83C1.54,14.42,2.08,15.0,2.62,15.0C2.62,15.0,13.42,15.0,13.42,15.0C13.96,15.0,14.5,14.42,14.5,13.83C14.5,13.83,14.5,3.92,14.5,3.92C14.5,3.33,13.96,2.75,13.42,2.75C13.42,2.75,2.62,2.75,2.62,2.75C2.08,2.75,1.54,3.33,1.54,3.92C1.54,3.92,1.5,13.83,1.5,13.83M1.54,5.67C1.54,5.67,14.5,5.67,14.5,5.67C14.5,5.67,1.54,5.67,1.54,5.67M5.32,1.0C5.32,1.0,5.32,4.5,5.32,4.5C5.32,4.5,5.32,1.0,5.32,1.0M10.72,1.0C10.72,1.0,10.72,4.5,10.72,4.5C10.72,4.5,10.72,1.0,10.72,1.0Z" stroke-linecap="round"/></svg>
+    );
     
     return (
         <div className="pt-[1rem] w-full">
-            <ul ref={projectsList} className="w-full snap-x snap-mandatory h-[20rem] overflow-x-scroll flex flex-row gap-[0.5rem] [&>li]:snap-center [&>li]:block [&>li]:h-full [&>li]:w-[20rem] [&>li]:border [&>li]:border-borderColor [&>li]:rounded-xl [&>li>span]:block [&>li>span]:p-[0.5rem] [&>li>div]:h-[15rem] [&>li>div]:w-[20rem] [&>li>div]:overflow-hidden [&>li>div]:rounded-t-xl [&>li>div>img]:h-full [&>li>div>img]:w-full [&>li>span>p]:first:font-medium">
+            <ul ref={projectsList} className="w-full overflow-y-scroll flex flex-row gap-[1rem] [&>li>a]:flex [&>li>a]:flex-col [&>li>a]:gap-[0.35rem] [&>li]:w-full [&>li]:border [&>li]:border-borderColor [&>li]:rounded-xl [&>li]:py-[0.5rem] [&>li]:px-[1rem] [&>li>a>span]:flex [&>li>a>span]:flex-row [&>li>a>span]:gap-[0.5rem] [&>li>a>span]:items-center [&>li>a>span>h3]:font-medium [&>li>a>p]:flex [&>li>a>p]:flex-row [&>li>a>p]:gap-[0.5rem] [&>li>a>p]:items-center [&>li>a>p]:text-[0.85rem] [&>li>a>p]:opacity-60 [&>li>a>span>p]:text-[0.85rem] [&>li>a>span>p]:text-[#006400] [&>li>a>span>p]:bg-[#00FF0020] [&>li>a>span>p]:border [&>li>a>span>p]:border-[#00FF0035] [&>li>a>span>p]:rounded-2xl [&>li>a>span>p]:px-[0.5rem]">
                 <li>
-                    <div><img src="%PUBLIC_URL%/../DiscordBOT-Handle.webp" alt="project handle"></img></div>
-                    <span>
-                        <p>Discord BOT</p>
-                        <p>Preview available soon...</p>
-                    </span>
-                </li>
-                <li>
-                    <div><img src="%PUBLIC_URL%/../OnlineKaraoke-Handle.webp" alt="project handle"></img></div>
-                    <span>
-                        <p>Online Karaoke</p>
-                        <p>Preview available soon...</p>
-                    </span>
+                    <NavLink to="/games/numberlock" target="_blank">
+                        <span>
+                            <h3>Number Lock &#x28;Game&#x29;</h3>
+                            <p>Public</p>
+                        </span>
+                        <p>{calendarIcon} October 2025</p>
+                    </NavLink>
                 </li>
             </ul>
         </div>
